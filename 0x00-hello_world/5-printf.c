dd2784a7483b77ab5c72803953c213e08732dd60
IBUKUNOLUWA <ibukunoluwa200131@gmail.com>
2023-08-11 22:42:08 +0100

    commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,41 +1,14 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
+ * main - the main function
  *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
+ * Return: always return 0
+ */
 
 int main(void)
 
 {
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
-
-}#include<stdio.h>
-
-/**
- *
- * * main - Entry point
- *
- * *
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-int main(void)
-
-{
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");
-
-		        return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 
 }
