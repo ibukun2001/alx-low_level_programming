7502b6bd5924f6d202b02d97c15b4e6e05dfd0fb
IBUKUNOLUWA <ibukunoluwa200131@gmail.com>
2023-08-11 21:40:54 +0100

    commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,21 +1,10 @@
-#include<stdio.h>
-
+#include <stdio.h>
 /**
- *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main - main function
+ * Return: always 0
+ */
 int main(void)
-
 {
-
-	        puts("\"Programming is like building a multilingual puzzle");
-
-		        return (0);
-
+	puts("\"Programming is like building a multilingual puzzle);
+	return(0);
 }
