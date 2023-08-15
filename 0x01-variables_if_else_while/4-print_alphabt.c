578f30c93f55e230f83bea663ccfe1c014488f52
Ibukunoluwa <ibukunoluwa200131@gmail.com>
2023-08-15 02:04:59 +0100

    commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- *  * main - prints the alphabet in lowercase,
- *   * followed by a new line, except q and e
- *    * Return: Aways 0 (Sucess)
- *     */
+ * main - prints the alphabet in lowercase,
+ * followed by a new line, except q and e
+ * Return: Aways 0 (Sucess)
+ */
 int main(void)
 {
 	char ch = 'a';
