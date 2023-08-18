c9630e655d87c668efb97540f8d9c9936ba846ea
Ibukunoluwa <ibukunoluwa200131@gmail.com>
2023-08-18 10:11:36 +0100

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ * main - contains the main function
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	for (n = 0; n < 10; n++)
+	{
+		printf("%d,n";)
+	}
+	printf("\n");
+	return (0);
+}
