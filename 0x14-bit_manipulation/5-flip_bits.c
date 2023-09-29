dc33813fd5df192b63e506ce540e23dc5becc081
ibukunoluwa <ibukunoluwa200131@gmail.com>
2023-09-29 03:53:46 +0100

    commit message

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+lude "main.h"
+
+/**
+ * flip_bits - it counts the number of bits to change
+ * to get from one number to another
+ * @n: the first number
+ * @m: the second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
